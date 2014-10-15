d10cb7ab26c297e650fdf74714f648caaeb37035
Hannes Weisbach <hannes.weisbach@mailbox.tu-dresden.de>
2014-10-15 16:20:58 +0200

    Add {c|r}{begin|end} since gcc doesnt know it.

---
+++ src/util.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace std {
+template <typename T> auto cbegin(const T &c) { return c.cbegin(); }
+template <typename T> auto crbegin(const T &c) { return c.crbegin(); }
+template <typename T> auto cend(const T &c) { return c.crbegin(); }
+template <typename T> auto crend(const T &c) { return c.crbegin(); }
+}
+
