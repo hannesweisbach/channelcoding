4a4524fd5eea3e409789aff15bedee07dcd399b4
Hannes Weisbach <hannes.weisbach@mailbox.tu-dresden.de>
2014-11-24 15:03:14 +0100

    Add hard_decision decoding algorithms for cyclic codes
    - PGZ
    - BM
    - EUKLID (Sugiyama)

---
+++ src/codes/hard_decision.h
@@ -0,0 +1,170 @@
+#pragma once
+
+#include <vector>
+#include <stdexcept>
+#include <iterator>
+#include <numeric>
+#include <string>
+
+#include "codes/codes.h"
+#include "gf/linear_equation_system.h"
+#include "protocol.h"
+
+namespace cyclic {
+
+struct peterson_gorenstein_zierler_tag : hard_decision_tag {
+  static std::string to_string() { return "PGZ"; }
+};
+struct berlekamp_massey_tag : hard_decision_tag {
+  static std::string to_string() { return "BM"; }
+};
+/* AKA Sugiyama algorithm */
+struct euklid_tag : hard_decision_tag {
+  static std::string to_string() { return "EUKLID"; }
+};
+/* TODO:
+ * L. R. Welch, E. Berlekamp, Error correction for algebraic block codes, US
+ *                            Patent, Number 4,633,470, 1986.
+ * G. Forney, Generalized minimum distance decoding, Information Theory, IEEE
+ *            Transactions on, vol.12, no.2, pp. 125-131, 1966.
+ * J. Jiang, K. R. Narayanan, Iterative Soft Decoding of Reed-Solomon Codes,
+ *                            IEEE Communications Letters, vol.8, no.4,
+ *                            pp.244-246, 2004.
+ * inverse-free BMAs
+ */
+
+template <typename Polynomial,
+          typename Element = typename Polynomial::element_type>
+Polynomial error_locator_polynomial(const std::vector<Element> &syndromes,
+                                    const std::vector<unsigned> &erasures,
+                                    peterson_gorenstein_zierler_tag) {
+  if (!erasures.empty())
+    throw std::runtime_error(
+        "The PGZ-Algorithm does not support erasure decoding");
+
+  /* TODO: this implementation suffers from decoder malfunction. Fix it.
+   * http://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=333881
+   */
+  for (unsigned v = syndromes.size() / 2; v; --v) {
+    linear_equation_system<Polynomial> eq_system;
+    for (auto it = std::cbegin(syndromes); it != std::cbegin(syndromes) + v;
+         ++it) {
+      Polynomial poly;
+      std::reverse_copy(it, it + v + 1, std::back_inserter(poly));
+      eq_system.push_back(poly);
+    }
+
+    // TODO Check if the determinant is non-zero.
+    try {
+      auto solution = eq_system.solution();
+      solution.push_back(Element(1));
+      return solution;
+    }
+    catch (const std::exception &e) {
+    }
+  }
+
+  /* note: this was supposed to solve decoder malfunction, but it is bullshit.
+   * if v < t, then multiple equation systems can be built. each must be
+   * solvable and yield the same result. otherwise, decoding failure must be
+   * declared.
+   * This is not an immediate problem anymore, since the corrected codeword's
+   * syndrome is checked again.
+   */
+  auto sigma = syndromes.front();
+  std::cout << sigma << std::endl;
+  for (auto it = std::begin(syndromes); it != std::end(syndromes) - 1; ++it) {
+    if (*it == Element(0))
+      throw decoding_failure("Cannot invert syndrome, it is 0.");
+    std::cout << *it << " " << Element(1) / *it << std::endl;
+    auto sigma_ = *(it + 1) * (Element(1) / *it);
+    std::cout << *(it + 1) << " " << sigma_ << std::endl;
+    if (sigma != sigma_) {
+      // std::cout << sigma << " " << sigma_ << std::endl;
+      throw decoding_failure("No solution found.");
+    }
+  }
+
+  return Polynomial({ sigma, Element(1) });
+}
+
+template <typename Polynomial,
+          typename Element = typename Polynomial::element_type>
+Polynomial error_locator_polynomial(const std::vector<Element> &syndromes,
+                                    const std::vector<unsigned> &erasures,
+                                    berlekamp_massey_tag) {
+  const auto fk = syndromes.size() / 2;
+  const auto rho = erasures.size();
+  Polynomial lambda({ Element(1) });
+  Polynomial b;
+  int l = erasures.size();
+
+  /* position of the erasure is the power of the element */
+  for (const auto &erasure : erasures)
+    lambda *= Polynomial({ Element(1), Element::from_power(erasure) });
+
+  b = lambda;
+
+  for (int i = rho; i < 2 * fk; i++) {
+    /* b = b * x; */
+    b *= Polynomial({ Element(0), Element(1) });
+    /* d = si + \sigma_j=1^l lambda_j * s_i-j; */
+    const auto delta =
+        std::inner_product(std::cbegin(lambda) + 1, std::cbegin(lambda) + l + 1,
+                           std::crend(syndromes) - i, syndromes.at(i));
+
+    if (delta) {
+      auto t = lambda + b * delta;
+      if (2 * l <= i + rho) {
+        b = lambda * delta.inverse();
+        l = i + rho - l + 1;
+      }
+      lambda = t;
+    }
+    protocol_bm(i, delta, lambda, l, b, std::false_type());
+  }
+
+  return lambda.reverse();
+}
+
+template <typename Polynomial,
+          typename Element = typename Polynomial::element_type>
+Polynomial error_locator_polynomial(const std::vector<Element> &syndromes,
+                                    const std::vector<unsigned> &erasures,
+                                    euklid_tag) {
+  const unsigned fk = syndromes.size() / 2;
+  const unsigned dmin = 2 * fk + 1;
+  const unsigned max = (2 * fk + erasures.size()) / 2;
+
+  Polynomial u({ Element(1) });
+  std::vector<Polynomial> w;
+  std::vector<Polynomial> r;
+  Polynomial s(syndromes);
+
+  for (const auto &erasure : erasures)
+    u *= Polynomial({ Element(1), Element::from_power(erasure) });
+
+  r.push_back(s * u);
+  r.push_back(Polynomial(dmin - 1, Element(0)));
+  r.back().push_back(Element(1));
+
+  w.push_back(u);
+  w.push_back(Polynomial({ Element(0) }));
+
+  for (int i = 1; r.back().degree() >= max; i++) {
+    auto next = r.at(i - 1) % r.at(i);
+    auto q = r.at(i - 1) / r.at(i);
+    w.push_back(w.at(i - 1) + q * w.at(i));
+    r.push_back(next);
+  }
+
+  /* I wish I had static_if ... */
+  protocol_euklid(r, w, std::false_type());
+
+  if (w.back().at(0) == Element(0))
+    throw decoding_failure("Cannot invert last element");
+
+  auto lambda = w.back() * w.back().at(0).inverse();
+  return lambda.reverse();
+}
+}
---
+++ src/codes/protocol.h
@@ -0,0 +1,118 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <ostream>
+#include <algorithm>
+#include <iterator>
+#include <clocale>
+#include <type_traits>
+
+#include "center.h"
+
+template <typename Polynomial>
+static void protocol_euklid(const std::vector<Polynomial> &r,
+                            const std::vector<Polynomial> &w, std::false_type) {
+}
+
+template <typename Polynomial>
+static void protocol_euklid(const std::vector<Polynomial> &r,
+                            const std::vector<Polynomial> &w, std::true_type) {
+  std::setlocale(LC_ALL, "en_US.UTF-8");
+
+  std::vector<std::string> r_;
+  std::vector<std::string> w_;
+  std::vector<std::string> q_;
+
+  auto poly2str = [](const auto &poly) {
+    std::ostringstream os;
+    os << poly;
+    return os.str();
+  };
+
+  std::transform(std::cbegin(r), std::cend(r), std::back_inserter(r_),
+                 poly2str);
+  std::transform(std::cbegin(w), std::cend(w), std::back_inserter(w_),
+                 poly2str);
+  std::transform(std::cbegin(r) + 1, std::cend(r), std::cbegin(r),
+                 std::back_inserter(q_),
+                 [&](const auto &first1, const auto &first2) {
+    auto q = first2 / first1;
+    return poly2str(q);
+  });
+
+  auto mb_strcmp = [](const std::string &lhs, const std::string &rhs) {
+    return mbstowcs(nullptr, lhs.c_str(), 0) <
+           mbstowcs(nullptr, rhs.c_str(), 0);
+  };
+  const auto max_w =
+      std::max_element(std::cbegin(w_), std::cend(w_), mb_strcmp);
+  const auto max_r =
+      std::max_element(std::cbegin(r_), std::cend(r_), mb_strcmp);
+  const auto max_q =
+      std::max_element(std::cbegin(q_), std::cend(q_), mb_strcmp);
+
+  const auto w_len = mbstowcs(nullptr, max_w->c_str(), 0);
+  const auto r_len = mbstowcs(nullptr, max_r->c_str(), 0);
+  const auto q_len = mbstowcs(nullptr, max_q->c_str(), 0);
+  const auto i_len = std::max(r.size() / 3, 2UL);
+
+  std::cout << std::endl << "Protocol EUKLID algorithm:" << std::endl;
+
+  std::cout << std::setw(i_len) << std::right << "i"
+            << " | ";
+  std::cout << std::setw(r_len) << centered("r(x)") << " | ";
+  std::cout << std::setw(q_len) << centered("q(x)") << " | ";
+  std::cout << std::setw(w_len) << centered("w(x)") << " |" << std::endl;
+
+  std::cout << std::setfill('-') << std::setw(i_len) << ""
+            << "-+-";
+  std::cout << std::setw(r_len) << ""
+            << "-+-";
+  std::cout << std::setw(q_len) << ""
+            << "-+-";
+  std::cout << std::setw(w_len) << ""
+            << "-+";
+  std::cout << std::endl << std::setfill(' ');
+
+  for (int i = 1; i < r.size(); i++) {
+    std::cout << std::setw(i_len) << std::right << i - 2 << " | ";
+    std::cout << std::setw(r_len) << centered(r_.at(i)) << " | ";
+    if (i < 2) {
+      std::cout << std::setw(q_len) << ""
+                << " | ";
+    } else {
+      std::cout << std::setw(q_len) << centered(q_.at(i - 1)) << " | ";
+    }
+    std::cout << std::setw(w_len) << centered(w_.at(i)) << " |" << std::endl;
+  }
+
+  std::cout << std::setfill('-') << std::setw(i_len) << ""
+            << "-+-";
+  std::cout << std::setw(r_len) << ""
+            << "-+-";
+  std::cout << std::setw(q_len) << ""
+            << "-+-";
+  std::cout << std::setw(w_len) << ""
+            << "-+";
+  std::cout << std::endl << std::setfill(' ');
+
+  std::cout << "Λ(x)_euklid = " << w.back() << " * " << w.back().at(0)
+            << "^-1 = " << w.back() * w.back().at(0).inverse() << std::endl;
+  std::cout << "v = " << w.back().degree() - w.at(0).degree() << std::endl
+            << std::endl;
+}
+
+template <typename Polynomial,
+          typename Element = typename Polynomial::element_type>
+void protocol_bm(const int i, const Element &delta, const Polynomial &lambda,
+                 const int &l, const Polynomial &b, std::false_type) {}
+
+template <typename Polynomial,
+          typename Element = typename Polynomial::element_type>
+void protocol_bm(const int i, const Element &delta, const Polynomial &lambda,
+                 const int &l, const Polynomial &b, std::true_type) {
+  std::cout << i << " | " << delta << " | " << lambda << " | " << l << " | "
+            << b << " " << std::endl;
+}
